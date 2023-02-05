aecfa0522ab47ad2baeaec616c69a95abecdd5aa
iamabrarsaifudheen <72561669+iamabrarsaifudheen@users.noreply.github.com>
2023-02-05 22:30:06 +0530

    Version 3

--- main.py
+++ main.py
@@ -21,8 +21,14 @@ app.exec()
 import sys
 from PySide6.QtWidgets import QApplication,QMainWindow,QPushButton
 from button_holder import ButtonHolder
-
 app=QApplication(sys.argv)
-window=ButtonHolder()
-window.show()
+def button_clicked():
+    print("You clicked the button")
+
+button=QPushButton("Press Me")
+
+button.clicked.connect(button_clicked)
+
+
+button.show()
 app.exec()
